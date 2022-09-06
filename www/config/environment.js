1416bb5e049e6588979226404b0d3b60d30c3b30
ahonder <42313685+ahonder@users.noreply.github.com>
2022-09-06 12:13:47 +0300

    Update environment.js

--- www/config/environment.js
+++ www/config/environment.js
@@ -15,14 +15,14 @@ module.exports = function(environment) {
   
       APP: {
         // API host and port
-        ApiUrl: '//192.168.178.27//',
+        ApiUrl: '//etcturk.sytes.net//',
   
         // HTTP mining endpoint
-        HttpHost: 'http://192.168.178.27',
+        HttpHost: 'http://etcturk.sytes.net',
         HttpPort: 8888,
   
         // Stratum mining endpoint
-        StratumHost: 'example.net',
+        StratumHost: 'etcturk.sytes.net',
         StratumPort: 8008,
   
         // The ETC network used (classic, mordor)
