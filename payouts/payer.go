27e90ad67a9dbedd93cae88cf7b7a5abd25507e9
ahonder <42313685+ahonder@users.noreply.github.com>
2022-09-06 13:34:12 +0300

    Update payer.go

--- payouts/payer.go
+++ payouts/payer.go
@@ -12,9 +12,9 @@ import (
 
 	"github.com/ethereum/go-ethereum/common/hexutil"
 
-	"github.com/yuriy0803/open-etc-pool-friends/rpc"
-	"github.com/yuriy0803/open-etc-pool-friends/storage"
-	"github.com/yuriy0803/open-etc-pool-friends/util"
+	"github.com/ahonder/etc-pool/rpc"
+	"github.com/ahonder/etc-pool/storage"
+	"github.com/ahonder/etc-pool/util"
 )
 
 const txCheckInterval = 5 * time.Second
